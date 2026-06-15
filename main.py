6d45cbfcc6842e575a6848813c8a2761a4c3d2ac
mukesh kumar <mukesh.kumar401514@gmail.com>
2026-06-15 11:32:31 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def hello_world():
+    return {"message": "hello world"}
+
+
+from fastapi.responses import FileResponse
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("favicon.ico")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
